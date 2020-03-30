717f84325d2113d81b3f8775e876dcd29812f720
sachin patwal <46517602+Sachinpatwal07@users.noreply.github.com>
2020-03-30 12:08:32 +0530

    solved preload audio issue.

--- index.js
+++ index.js
@@ -1,69 +1,82 @@
+var S = new Audio("sounds/tom-1.mp3");
+
+var A = new Audio("sounds/snare.mp3");
+
+var C = new Audio("sounds/tom-3.mp3");
+
+var H = new Audio("sounds/tom-4.mp3");
+
+var I = new Audio("sounds/kick-bass.mp3");
+
+var N = new Audio("sounds/crash.mp3");
+
+var L = new Audio("sounds/tom-2.mp3");
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-    soundPlayer(this.innerHTML);
-    annimation(this.innerHTML);
+  soundPlayer(this.innerHTML);
 
-  });
+  annimation(this.innerHTML);
 
-  document.addEventListener("keypress", function(event) {
+});
 
+document.addEventListener("keypress", function(event) {
 
-    soundPlayer(event.key);
-    annimation(event.key);
+  soundPlayer(event.key);
 
-  })
+  annimation(event.key);
 
-
-
-  function soundPlayer(a) {
+})
 
 
-    switch (a) {
-      case "s":
-        var sound = new Audio("sounds/tom-1.mp3");
-        sound.play();
-        break;
-      case "a":
-        var sound = new Audio("sounds/snare.mp3")
-        sound.play();
-        break;
-      case "c":
-        var sound = new Audio("sounds/tom-3.mp3");
-        sound.play();
-        break;
-      case "h":
-        var sound = new Audio("sounds/tom-4.mp3");
-        sound.play();
-        break;
-      case "i":
-        var sound = new Audio("sounds/kick-bass.mp3");
-        sound.play();
-        break;
-      case "n":
-        var sound = new Audio("sounds/crash.mp3");
-        sound.play();
-        break;
-      case "l":
-        var sound = new Audio("sounds/tom-2.mp3");
-        sound.play();
-        break;
 
-    }
+function soundPlayer(a) {
 
 
-  }
-
-  function annimation(key) {
-
-    var z = document.querySelector("." + key);
-    z.classList.add("pressed");
-    setTimeout(function() {
-      z.classList.remove("pressed");
-    }, 100);
+  switch (a) {
+    case "s":
+                S.src =  S.src;
+                S.play();
+                break;
+    case "a":
+                A.src = A.src;
+                A.play();
+                break;
+    case "c":
+                C.src = C.src;
+                C.play();
+                break;
+    case "h":
+                H.src = H.src;
+                H.play();
+                break;
+    case "i":
+                I.src = I.src;
+                I.play();
+                break;
+    case "n":
+                N.src = N.src;
+                N.play();
+                break;
+    case "l":
+                L.src = L.src;
+                L.play();
+                break;
 
   }
 
 
+}
+
+function annimation(key) {
+
+var z = document.querySelector("." + key);
+
+  z.classList.add("pressed");
+
+  setTimeout(function() { z.classList.remove("pressed");}, 100);
+
+}
 }
